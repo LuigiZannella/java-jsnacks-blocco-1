94e443ad2753ad8eea82d2d7d0d82271d780624e
LuigiZannella <zannellaluigi@outlook.com>
2022-06-13 11:04:49 +0200

    JSnacks finito

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -1,5 +1,22 @@
 package jsnacks;
 
+import java.util.Scanner;
+
 public class JSnack1 {
 
+	public static void main(String[] args) {
+	
+
+	int numero;
+	int somma = 0;
+	Scanner nm = new Scanner(System.in);
+	System.out.println("inseriti 5 numeri ");
+	
+	for(int i = 0; i < 5; i++) {
+		numero = nm.nextInt();
+		somma = somma + numero;
+	}
+	System.out.println("La somma dei 5 numeri è: " + somma);
+
+	nm.close();}
 }
