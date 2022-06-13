d254955ae927a8a1a8cc5f7923f8bc2677075aff
LuigiZannella <zannellaluigi@outlook.com>
2022-06-13 12:04:04 +0200

    JSnack3 completo

--- src/jsnacks/JSnack3.java
+++ src/jsnacks/JSnack3.java
@@ -1,10 +1,21 @@
 package jsnacks;
 
+import java.util.Random;
+
 public class JSnack3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	String [] Nomi = {"Luigi","Giuseppe","Antonio","Francesco"};
+    String [] Cognomi = {"Cobucci","Sparadice","Creations","Capaccio"};
+
+	Random random = new Random ();
+	
+	for(int i = 0; i < 4; i++) {
+		int random1 = random.nextInt(4);
+	    int random2 = random.nextInt(4);
+	    System.out.println(Nomi[random1] + " " + Cognomi[random2]);
 	}
-
+	
+}
 }
